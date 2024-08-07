d239d83f6ea3d54c29df17f1efa362a9b51cbd2f
Makvana-karan <karanmakwana402@gmail.com>
2024-08-07 09:28:06 +0530

    save

--- server/index.js
+++ server/index.js
@@ -8,8 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //error handel
 app.use((err, req, res, next) => {
@@ -22,7 +21,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
     message: "Hello",
   });
